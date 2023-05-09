2e2af78fa4862bfe2df67dd4f7e08e5b734e16ef
mulchus <mulch@list.ru>
2023-05-09 20:21:26 +0300

    Delete 'skip_updates', rename variable

--- main.py
+++ main.py
@@ -33,16 +33,16 @@ async def send_welcome(message: types.Message):
             response = requests.get(url, headers=headers, params=payload, timeout=timeout)
             response.raise_for_status()
             logging.info(response.url)
-            response = response.json()
-            logging.info(json.dumps(response, indent=2, ensure_ascii=False))
-            if response['status'] == 'timeout':
-                payload['timestamp'] = response['timestamp_to_request']
+            devman_response_in_format = response.json()
+            logging.info(json.dumps(devman_response_in_format, indent=2, ensure_ascii=False))
+            if devman_response_in_format['status'] == 'timeout':
+                payload['timestamp'] = devman_response_in_format['timestamp_to_request']
                 await message.answer('Нет обновлений.')
-            elif response['status'] == 'found':
-                payload['timestamp'] = response['last_attempt_timestamp']
-                reply_message = f'Преподаватель проверил работу "{response["new_attempts"][0]["lesson_title"]}"!\n' \
-                                f'{response["new_attempts"][0]["lesson_url"]}\n'
-                if response["new_attempts"][0]["is_negative"]:
+            elif devman_response_in_format['status'] == 'found':
+                payload['timestamp'] = devman_response_in_format['last_attempt_timestamp']
+                reply_message = f'Преподаватель проверил работу "{devman_response_in_format["new_attempts"][0]["lesson_title"]}"!\n' \
+                                f'{devman_response_in_format["new_attempts"][0]["lesson_url"]}\n'
+                if devman_response_in_format["new_attempts"][0]["is_negative"]:
                     reply_message = f'{reply_message} К сожалению в работе нашлись ошибки.'
                 else:
                     reply_message = f'{reply_message} Преподавателю все понравилось. ' \
@@ -57,4 +57,4 @@ async def send_welcome(message: types.Message):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=False)
+    executor.start_polling(dp)
